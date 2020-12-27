a3bba75ca4a5a402921c48f0b3ac427b0eb6aa91
gruteke <rgruteke@yahoo.com>
2020-12-27 14:23:47 +0000

    update

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -42,7 +42,7 @@ public class MainView extends VerticalLayout {
     public MainView(@Autowired GreetService service) {
 
         // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
+        TextField textField = new TextField("Your first name");
         textField.addThemeName("bordered");
 
         // Button click listeners can be defined as lambda expressions
